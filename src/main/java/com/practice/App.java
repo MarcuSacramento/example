2c9ed48c4b7a6afb596272fc320c9fc588be5c1b
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2018-06-15 15:27:44 -0300

    Update App.java

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sam!" );
+        System.out.println( "Hello Sam!"+"teste de     alteração" );
     }
 }
