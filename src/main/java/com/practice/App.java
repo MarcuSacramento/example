5dbe1bda1d9a8ea96db2c93ec0cf8bdf42a3f968
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2018-06-15 15:30:06 -0300

    Commit proposital de erro

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -6,6 +6,12 @@ package com.practice;
  */
 public class App
 {
+    
+    int target = -5;
+int num = 3;
+
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num; // Noncompliant; target = 3
     public static void main( String[] args )
     {
         System.out.println( "Hello Sam!"+"teste de     alteração" );
